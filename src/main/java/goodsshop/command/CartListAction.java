cac4749d113e51c401bd741860da9f4dc6279315
Lee-1sj <sukjin0112@naver.com>
2024-07-02 20:22:36 +0900

    update project

--- src/main/java/goodsshop/command/CartListAction.java
+++ src/main/java/goodsshop/command/CartListAction.java
@@ -1,16 +1,37 @@
 package goodsshop.command;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import goodsshop.bean.CartDBBean;
+import goodsshop.bean.CartDataBean;
 import goodsshop.process.CommandAction;
 
 public class CartListAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		request.setCharacterEncoding("utf-8");
+		String buyer = request.getParameter("buyer");
+		
+		List<CartDataBean> cartLists = null;
+		int count = 0;
+
+		//해당 buyer의 장바구니 목록의 수를 얻어냄
+		CartDBBean goodsProcess = CartDBBean.getInstance();
+		count = goodsProcess.getListCount(buyer);
+		
+		if(count > 0){//해당 buyer의 장바구니 목록이 있으면 수행
+			//해당 buyer의 장바구니 목록을 얻어냄
+			cartLists = goodsProcess.getCart(buyer, count);
+			request.setAttribute("cartLists", cartLists);
+		}
+		
+		request.setAttribute("count", Integer.valueOf(count));
+		request.setAttribute("type", Integer.valueOf(1));
+		return "/cart/cartList.jsp";
 	}
 
 }
